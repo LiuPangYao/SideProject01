5a17b63d68f3fe16ec990d663d58037004433552
liubangyao <a.today.13th.blackdays@gmail.com>
2021-02-16 22:14:01 +0800

    update MainActivity.java update code to github

--- app/src/main/java/com/froggy/nsfw_gbike/MainActivity.java
+++ app/src/main/java/com/froggy/nsfw_gbike/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         test();
+        // update code to github
         // 20210216 update Github code for blog
         // update test
         // test
