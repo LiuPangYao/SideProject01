9446687f088526f84d5714930ea7efa807e21a38
liubangyao <a.today.13th.blackdays@gmail.com>
2021-02-17 21:42:18 +0800

    add new file

---
+++ app/src/main/java/com/froggy/nsfw_gbike/GithubTest.java
@@ -0,0 +1,13 @@
+package com.froggy.nsfw_gbike;
+
+/**
+ * Copyright 2021 想發財娛樂
+ * <p>
+ * Date 2021 - 02 - 17
+ * Author : 紅色維尼
+ * Descriptor :
+ * Fix Item : nsfw_gbike
+ **/
+
+public class GithubTest {
+}
