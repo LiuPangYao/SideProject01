efe86cf1f550f61f4b0219444c266aebb86582fc
LiuPangYao <a.today.13th.blackdays@gmail.com>
2021-02-17 22:31:58 +0800

    Update MainActivity.java

--- app/src/main/java/com/froggy/nsfw_gbike/MainActivity.java
+++ app/src/main/java/com/froggy/nsfw_gbike/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void test() {
         // update remote test
+        // test conflict1
         Log.d(TAG, "onCreate: ");
     }
 
