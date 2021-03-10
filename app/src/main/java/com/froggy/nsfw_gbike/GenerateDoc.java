f72d1912fad2b13bcf6c39e1b7100e3f34790d44
liubangyao <a.today.13th.blackdays@gmail.com>
2021-03-10 20:54:25 +0800

    fix(APP 架構): Add 架構修正
    1. 刪除沒有使用到的檔案

---
+++ app/src/main/java/com/froggy/nsfw_gbike/GenerateDoc.java
@@ -0,0 +1,33 @@
+package com.froggy.nsfw_gbike;
+
+/**
+ * <p>Project Name : nsfw_gbike <p/>
+ * <p>File Name : GenerateDoc <p/>
+ * <p>Describe : <p/>
+ * <p>Company : 想發財娛樂 - 創意發想組 ＠2021 <p/>
+ * <p>date 2021 - 03 - 08 <p/>
+ *
+ * @author : 紅色維尼
+ * @version : v 1.0.0
+ **/
+public class GenerateDoc {
+
+    /**
+     * GenerateTestDoc 測試用
+     * @since : v 0.0.2
+     */
+    public void GenerateTestDoc() {
+
+    }
+
+    /**
+     * GenerateTestDoc 測試用
+     * @since : v 0.0.9
+     * @param : [a, b] a 為字數 b 為路徑
+     */
+    public void GenerateTestDoc(int a, String b) {
+
+    }
+}
+
+
--- app/src/main/java/com/froggy/nsfw_gbike/MainActivity.java
+++ app/src/main/java/com/froggy/nsfw_gbike/MainActivity.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-/**
- * 20210306 更新
- */
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
@@ -18,13 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         testABC();
-        // update code to github
-        // test
-        // 20210216 update Github code for blog
-        // update test
-        // test
-        // third
-
+        createNBADataBase(2, 4);
         testBuildConfigSetting();
     }
 
@@ -32,22 +24,34 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "WEB URL CASH: " + BuildConfig.WEB_URL_CASH);
     }
 
-    private void testABC() {
-        // update remote test
-        // commit conflict log
-        Log.d(TAG, "onCreate: ");
+    /**
+     * prepareGenerateDoc 測試用
+     * @since : v 0.0.6
+     * @param : [a] 總投籃量
+     * @return : int
+     */
+    private int prepareGenerateDoc(int a) {
+        int hitRate = 100;
+        return hitRate;
     }
 
     /**
-     * @Date : 2/13/21
-     * @Title  : test
-     * @Author : 紅色維尼
-     * @Description :
-     * @Return : void
-     * @Parameter : [a, b]
+     * testABC 測試用
+     * @since : v 0.0.5
      */
-    private void test(int a, int b) {
-        Log.d(TAG, "test: ");
+    private void testABC() {
+
+        Log.d(TAG, "testABC()");
+    }
+
+    /**
+     * For 測試用 {@link MainActivity#testABC()}
+     * @see MainActivity#testABC()
+     * @since : v 0.0.5
+     * @param : [a 為進球數, b 命中率]
+     */
+    private void createNBADataBase(int a, int b) {
+        Log.d(TAG, "test");
     }
 
 }
